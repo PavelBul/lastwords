59e74a59c648b4b98c3a584e94418d5abcc7155a
Pavel <bulgarevich_pavel@rambler.ru>
2014-04-15 22:06:00 +0400

    Update

--- classes/logs.php
+++ classes/logs.php
@@ -1,19 +1,11 @@
 <?php
-/**
- * Author: Bpo-scripts.ru
- * Date: 13.04.14
- * Time: 23:51
- * Email: mail@bpo-scripts.ru
- */
 
 class Logs {
-$text = "";
-$lo = fopen("log.txt","wr");
-  function zap()
+
+  public function zap($text)
   {
-fwrite($lo, $text);
-
-
+     $file = fopen('logs.txt',"wr");
+     fwrite($file, $text);
   }
 
 }
