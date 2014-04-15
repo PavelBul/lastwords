d8f88cc19256f3f833325b8f46ad0fa2aa9a6774
Pavel <bulgarevich_pavel@rambler.ru>
2014-04-16 00:17:20 +0400

    Update

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
   require("classes/system.php");
+
   //Вьюхи
-  switch($config->action){
+  switch($system->action){
       default:
           echo $user->getId();
           echo $user->getStatus();
 
           $data['titleText'] = 'Добро пожаловать!';
           $system->loadView('header',$data);
-
           $system->loadView('index');
           $system->loadView('footer');
       break;
